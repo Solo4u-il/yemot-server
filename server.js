3552979bd61f61a67acbc0e0e448173e2facec17
Solo4u-il <miritzilum@gmail.com>
2026-06-13 23:50:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/clicker', (req, res) => {
-    res.send("read=t-ברוכים הבאים למערכת הקליקרים אונליין&=max=1");
+    // פקודה שמורה לימות המשיח להקריא טקסט ממוחשב ולהמתין להקשת מקש אחד
+    res.send("read=t-ברוכים הבאים למערכת הקליקרים אונליין. נא להקיש אחת לאישור.=1,1,1,Static");
 });
 
 app.listen(process.env.PORT || 3000, () => {
