61fbc31c3669d35e8a1b1493cc8bce39ae9be8e1
Solo4u-il <miritzilum@gmail.com>
2026-06-14 15:44:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,22 +15,26 @@ app.get('/clicker', (req, res) => {
         currentQuestionId++; 
         votedUsers.clear(); 
         console.log(`[מנחה] המנחה עבר לשאלה מספר: ${currentQuestionId}! הרשימה אופסה.`);
+        
+        // חוזרים להתחלה, לקובץ 000
         return res.send("go_to_folder=/1");
     }
 
     // 2. הגנה מפני הצבעה כפולה (רמאות)
     if (votedUsers.has(userPhone)) {
         console.log(`[חסום] ${userPhone} ניסה להצביע שוב ונחסם.`);
-        // מחזיר אותך לשלוחה 1
-        return res.send("go_to_folder=/1");
+        
+        // המשתמש כבר הצביע! אנחנו מעבירים אותו לשלוחה 1, אבל פוקדים על המערכת להפעיל את קובץ api_002 (השקט)
+        return res.send("go_to_folder=/1&api_index=002");
     }
 
-    // 3. קליטת הצבעה פעם ראשונה
+    // 3. קליטת הצבעה פעם ראשונה (הצלחה)
     if (userChoice) {
         votedUsers.add(userPhone); 
         console.log(`[הצבעה נקלטה] שאלה ${currentQuestionId} | טלפון: ${userPhone} | תשובה: ${userChoice}`);
-        // מחזיר אותך לשלוחה 1
-        return res.send("go_to_folder=/1");
+        
+        // ההצבעה נקלטה! אנחנו מעבירים אותו לשלוחה 1, אבל פוקדים על המערכת להפעיל את קובץ api_001 (הביפ)
+        return res.send("go_to_folder=/1&api_index=001");
     }
 
     res.send("go_to_folder=/1");
