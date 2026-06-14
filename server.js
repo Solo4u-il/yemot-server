88c1c0c1d10265e0409b84496cc6d7e7e6b5999f
Solo4u-il <miritzilum@gmail.com>
2026-06-14 14:10:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ const express = require('express');
 const app = express();
 
 app.get('/clicker', (req, res) => {
-    // ימות המשיח שולחים את מספר הטלפון וההקשה של המאזין
-    const userChoice = req.query.ApiData;
-    const userPhone = req.query.ApiPhone;
+    // ימות המשיח שולחים את התשובה בפרמטר שהגדרנו ב-ext.ini
+    const userChoice = req.query.user_ans;
+    const userPhone = req.query.ApiPhone; // מספר הטלפון מגיע אוטומטית כברירת מחדל
 
-    if (!userChoice) {
-        // כניסה ראשונה: משמיעים את השאלה ומחכים לספרה אחת (1,1) ללא צורך בסולמית (no)
-        res.send("read=t-נא להקיש את מספר התשובה שלכם.=1,1,7,no");
-    } else {
-        // המאזין הקיש! הנתון נתפס בשרת בשביל מסך המנחה שלך
-        console.log(`[קליקר אונליין] הטלפון: ${userPhone} | הקיש תשובה: ${userChoice}`);
-
-        // כדי שלא יחזור לתפריט הראשי, אנחנו שולחים פקודת סיום שקטה שמשאירה אותו בשלוחה או מנתקת
-        res.send("id_list=t-תשובתך נקלטה.&תחזיר=hangup");
+    if (userChoice) {
+        // הדפסה חלקה בזמן אמת לשרת (עבור מסך המנחה שלך!)
+        console.log(`[קליקר אונליין] הטלפון: ${userPhone} | בחר בתשובה: ${userChoice}`);
     }
+
+    // בסיום, השרת אומר למערכת להשמיע הודעה קצרה ולנתק (hangup) כדי שלא יחזור לתפריט הראשי
+    res.send("id_list=t-תשובתך נקלטה, תודה.&תחזיר=hangup");
 });
 
 app.listen(process.env.PORT || 3000, () => {
