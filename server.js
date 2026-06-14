0328d9f187ad74a12af157b0f8d944a2fad60f17
Solo4u-il <miritzilum@gmail.com>
2026-06-14 10:06:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/clicker', (req, res) => {
-    // קבלת ההקשה של המשתמש מהפרמטרים שימות המשיח שולחים
-    const userChoice = req.query.ApiData;
+    // במצב של api_get_input, המקש מגיע במשתנה ApiGetInput
+    const userChoice = req.query.ApiGetInput;
 
     if (!userChoice) {
-        // אם אין בחירה, נשמיע את השאלה
-        res.send("read=t-מה בירת ישראל? אחת תל אביב, שתיים ירושלים.=1,1");
+        // פעם ראשונה בשלוחה - משמיעים רק את השאלה כטקסט רגיל
+        res.send("id_list=t-מה בירת ישראל? אחת תל אביב, שתיים ירושלים.&");
     } else {
-        // אם יש בחירה, נבדוק אותה
+        // המשתמש הקיש ספרה - בודקים ומחזירים תשובה
         if (userChoice === "2") {
             res.send("id_list=t-כל הכבוד! ירושלים היא התשובה הנכונה!&");
         } else {
